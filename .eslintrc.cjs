5adb4ef57af723a0641742b0eab5deb393722e20
brysonpdavis <brysonpd@gmail.com>
2023-06-29 07:26:02 -0400

    restore dotenv

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-misused-promises": ["error", {checksVoidReturn: false}]
+    "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }]
   },
 };
 
