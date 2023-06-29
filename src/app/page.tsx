5e4d1ef5de4e69a45fbbdc5c3719dc5e64c17be8
brysonpdavis <brysonpd@gmail.com>
2023-06-29 08:13:12 -0400

    add log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export default async function Home() {
 
     const ps = await getPosts()
 
+    console.log('these are the posts: ', ps)
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
