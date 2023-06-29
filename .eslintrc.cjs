9e9c5225cad131890a4e00f79c6849e10a349974
brysonpdavis <brysonpd@gmail.com>
2023-06-29 07:24:37 -0400

    no-misused-promises -> checksVoidReturn: false

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-misused-promises": ["warn"]
+    "@typescript-eslint/no-misused-promises": ["error", {checksVoidReturn: false}]
   },
 };
 
