30be149c0854c8efc48ef1683d1914667eb1e4c1
brysonpdavis <brysonpd@gmail.com>
2023-06-30 13:23:43 -0400

    remove apostrophe

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({ params }: { params: { postId: string } }) {
     const post = (await db.select().from(posts).where(eq(posts.id, Number(params.postId)))).at(0)
 
     if (!post) {
-        return <div>couldn't find that post</div>
+        return <div>couldnt find that post</div>
     }
     return <div className="p-4"><pre>
         {JSON.stringify(post, undefined, 4)}
