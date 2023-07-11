d355518e5107887e786f541a345d0340d75baa4a
brysonpdavis <brysonpd@gmail.com>
2023-07-11 07:16:20 -0400

    update runtime for new route

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,9 @@
 import Client from "./client"
 import { parseMarkdown } from "./actions"
 
+export const runtime = "edge"
+export const revalidate = 0
+
 export default async function New() {
-    return <Client parseMarkdown={parseMarkdown}/>
+    return <Client parseMarkdown={parseMarkdown} />
 }
- 
