7f47d498682030898594068f18df7cfd016c393e
brysonpdavis <brysonpd@gmail.com>
2023-07-11 07:33:03 -0400

    add extended markdown features

--- src/lib/parse-markdown.ts
+++ src/lib/parse-markdown.ts
@@ -1,5 +1,6 @@
 import { remark } from "remark"
 import remarkHtml from "remark-html"
+import remarkGfm from "remark-gfm"
 
 export async function parseMarkdown(md: string) {
     "use server"
@@ -7,6 +8,7 @@ export async function parseMarkdown(md: string) {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         .use(remarkHtml, { sanitize: true })
+        .use(remarkGfm)
         .process(md)
 
     return processedContent.toString()
